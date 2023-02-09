51f7f42b9c1b6389024ee28ab8ebda1382c57dd6
guaqueta <Camilo Guaqueta>
2023-02-08 17:27:29 -0800

    Clean up config namespace, add some docs, and use mount

--- src/clj/kitchen/config.clj
+++ src/clj/kitchen/config.clj
@@ -1,4 +1,11 @@
 (ns kitchen.config
-  (:require [cprop.core]))
+  "Manage the configuration for the system. By default `cprop` reads from a file
+  called `config.edn` which is expected to be in the resource-path.
 
-(def env (cprop.core/load-config))
+  We use `mount` to manage state lifecycles. For config `start` just means
+  reading the config file, and there is nothing to `stop`. Nevertheless, it is
+  convenient to reload the config whenever we restart the system."
+  (:require [cprop.core]
+            [mount.core :as mount]))
+
+(defstate env (cprop.core/load-config))
