599f2540cb6ebfe07602644ed22f01681155dd65
guaqueta <Camilo Guaqueta>
2023-02-08 17:22:24 -0800

    Clean up customer namespace and add some docs

--- src/clj/kitchen/customer.clj
+++ src/clj/kitchen/customer.clj
@@ -1,37 +1,42 @@
 (ns kitchen.customer
-  (:require [clojure.core.async :as async]
-            [clojure.tools.logging.readable :as log]
-            [kitchen.kitchen :as kitchen]
-            [clojure.data.json :as json]
-            [kitchen.config :as config]
-            [clojure.java.io :as io]))
+  "Emulate customers placing orders on the kitchen.
+
+  Order data is expected to be in a resource file. The filename is
+  configurable as (config/env :orders-json).
+
+  Orders are placed at a configurable rate. The relevant config variable
+  is :customer-wait-between-orders, which is in milliseconds."
+  (:require
+   [clojure.core.async :as async]
+   [clojure.data.json :as json]
+   [clojure.java.io :as io]
+   [kitchen.config :as config]
+   [kitchen.kitchen :as kitchen]))
 
 (def orders
+  "Reads orders from the resource specified in the config."
   (json/read-str
    (slurp (io/resource (config/env :orders-json)))
-   :key-fn keyword)
-  #_(let [base-orders (json/read-str
-                     (slurp (io/resource (config/env :orders-json)))
-                     :key-fn keyword)]
-    (mapcat (comp shuffle #(map (fn [order] (assoc order :id (str (java.util.UUID/randomUUID)))) %))
-            (repeat 10 base-orders))))
+   :key-fn keyword))
+
+(defn gen-orders 
+  "This is just a helper function for interactive testing. Using `orders` as a
+  reference, it generates `n` orders with random ids"
+  [n]
+  (take n (repeatedly (fn [] (assoc (rand-nth orders) :id (str (java.util.UUID/randomUUID)))))))
 
 (defn place-orders
   "Emulates customers placing orders on the system. Orders are placed
-  asynchronously (i.e. without waiting for a reply), at a fixed, configurable
-  rate governed by :customer-wait-between-orders. Executes in a separate
-  thread so as not to block the caller while sleeping between orders.
+  asynchronously (i.e. without waiting for a reply), at a fixed rate governed
+  by config/env :customer-wait-between-orders. Returns immediately.
 
-  Note we are deliberately not respecting back-pressure here. We are emulating
-  orders coming in at a rate which the rest of the system cannot control. TODO
-  is this really what I want"
-  [n]
-  (let [wait-time 10; (config/env :customer-wait-between-orders)
-        ]
-    (doseq [order (take n orders)] ;; XXX
-      (async/<!! (async/timeout wait-time))
-      ;; TODO error handling, rationale for not respecting backpressure
-      (log/info "Customer placing order")
-      (async/put! kitchen/orders-ch order)
-      #_(async/>!! kitchen/orders-ch order) ;;xxx
-      )))
+  Note we deliberately don't respect back-pressure (i.e. we use async/put! to
+  place orders). This is because we are emulating orders coming in at a rate
+  which the rest of the system cannot control. In other words, the kitchen can
+  drop orders, but it can't ask customers to wait."
+  ([] (place-orders (count orders)))
+  ([n] (let [wait-time (config/env :customer-wait-between-orders)]
+         (async/go
+           (doseq [order (take n orders)]
+            (async/<!! (async/timeout wait-time))
+            (async/put! kitchen/orders-ch order))))))
