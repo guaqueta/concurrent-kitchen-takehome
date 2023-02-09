fd2a78e838b72fe03e22b9e0869883ca0416cac2
guaqueta <Camilo Guaqueta>
2023-02-08 20:56:37 -0800

    Rollback using mount for config.

--- src/clj/kitchen/config.clj
+++ src/clj/kitchen/config.clj
@@ -1,11 +1,6 @@
 (ns kitchen.config
   "Manage the configuration for the system. By default `cprop` reads from a file
-  called `config.edn` which is expected to be in the resource-path.
+  called `config.edn` which is expected to be in the resource-path. "
+  (:require [cprop.core]))
 
-  We use `mount` to manage state lifecycles. For config `start` just means
-  reading the config file, and there is nothing to `stop`. Nevertheless, it is
-  convenient to reload the config whenever we restart the system."
-  (:require [cprop.core]
-            [mount.core :as mount]))
-
-(defstate env (cprop.core/load-config))
+(def env (cprop.core/load-config))
