173070d8b1aa8f8932050d4d33b880c40c171bab
guaqueta <Camilo Guaqueta>
2023-02-09 01:29:35 -0800

    Update docs, make functions private

--- src/clj/kitchen/kitchen.clj
+++ src/clj/kitchen/kitchen.clj
@@ -1,26 +1,16 @@
 (ns kitchen.kitchen
   "Emulate a kitchen that receives, processes, and delivers orders.
-
-  There are two services: the kitchen-service and the courier-service. The
-  kitchen-service receives orders on the `orders-ch` channel, cooks them,
-  dispatches couriers, and hands off cooked orders to arriving couriers. It
-  has internal state (the pick-up-area) to hold orders while waiting for
-  couriers to pickup. The pick-up-area has limited capacity, which is
-  configurable via the :shelf-capacity value in config.edn. If there are too
-  many incoming orders, eventually some orders will be dropped and the courier
-  will fail to pick them up.
-
-  The courier-service receives dispatch requests and schedules couriers to go
-  pick up orders. It also delivers orders that have been picked up. The
-  couriers wait a random amount of time before pickup, which is configurable
-  via the :courier-minimum-wait-time and :courier-maximum-wait-time values in
-  config.edn.
-
-  We use `mount` to manage the service lifecycle."
-  (:require [clojure.core.async :as async]
-            [clojure.tools.logging.readable :as log]
-            [kitchen.config :as config]
-            [mount.core :as mount]))
+ 
+  This namespace provides `kitchen`, a fn that returns an asynchronous machine
+  that emulates a kitchen. The kitchen accepts orders on the order-ch, and
+  returns finished orders on the delivery-ch. The kitchen has internal state
+  to emulate a shelf where cooked orders are stored, and a courier dispatch
+  service that schedules couriers to pick-up food from the shelf and deliver
+  it to the customer."
+  (:require
+   [clojure.core.async :as async]
+   [clojure.tools.logging.readable :as log]
+   [kitchen.config :as config]))
 
 (defn- cook
   "Cook the order. Essentially a placeholder for presumably more complex logic
@@ -28,7 +18,7 @@
   [order]
   (assoc order :cooked true))
 
-(defn make-empty-pick-up-area
+(defn- make-empty-pick-up-area
   "Returns a pick-up-area with no dishes on it. A pick-up-area is a map of
   temperatures to shelves, where each shelf in turn is a map of order ids to
   orders. We represent shelves as maps so that we can efficiently hand-off
@@ -51,7 +41,7 @@
                    (count v))])
              pick-up-area)))
 
-(defn put-on-pick-up-area
+(defn- put-on-pick-up-area
   "Put the given order on the best available shelf of the pick-up-area.
 
   First the shelf of the appropriate temperature. If that fails, try to put
@@ -90,7 +80,7 @@
                  :action :discarded
                  :affected-order discard-order})))))
 
-(defn pickup-order
+(defn- pickup-order
   "Remove the desired order from the pick-up-area. Returns a vector
   of [pick-up-area order].
 
@@ -110,7 +100,7 @@
       :else
       [pick-up-area (assoc order :pickup-successful false)])))
 
-(defn sample-courier-wait-time
+(defn- sample-courier-wait-time
   "Generate a random time for a courier to wait before going to pick up its
   order. The distribution is governed by the values in config/env"
   []
@@ -118,8 +108,42 @@
         max-time (config/env :courier-maximum-wait-time)]
     (+ min-time (Math/round (* (rand) (- max-time min-time))))))
 
-(defn get-kitchen
-  "TODO"
+(defn kitchen
+  "Returns a map of async channels that allow the caller to interact with a
+  machine that emulates a kitchen.
+
+  Takes a `delivery-ch` channel as argument. After orders are cooked and
+  picked up by couriers they will be put on this channel. By providing it as
+  an argument the caller is free to choose how the channel is buffered. The
+  `kitchen` doesn't respect backpressure on this channel and will `put!`
+  completed orders on it.
+
+  Orders are maps with, at minimum, :id and :temp keys. We assume :id is
+  unique, and that :temp is one of hot, cold, or frozen. Other keys are
+  allowed. As the order progresses through the kitchen it will gain :cooked
+  and :pickup-successful keys as well, with boolean values.
+
+  Orders are cooked and put in the pick-up-area. This area contains shelves at
+  different temperatures to hold different orders. See `put-on-pick-up-area`
+  for details. A courier is scheduled to come pickup completed orders. Once
+  the courier arrives, it delivers the order to `delivery-ch`.
+
+  The return map includes:
+
+  - stop: Any value put on this channel will cause the go-loop to halt on its
+  next iteration. Note this won't halt a loop that is parked waiting.
+
+  - report: For testing and debugging. A value on this channel will cause the
+  machine to log its internal state.
+
+  - orders: Orders placed here will be processed and delivered.
+
+  - end-orders: A value on this channel indicates that no more orders are
+  incoming. Once the kitchen completes all its deliveries it will close the
+  delivery-ch to indicate that it is finished.
+
+  - delivery: For convenience, we include the delivery-ch argument in the
+  return value."
   [delivery-ch]
   (let [stop-ch (async/chan)
         report-ch (async/chan)
@@ -139,6 +163,7 @@
                                (log/info "REPORT" pick-up-area couriers orders-ended)
                                [pick-up-area couriers orders-ended])
 
+                   ;; Handle incoming orders
                    orders-ch
                    ([order]
                     (log/info "order received" order pick-up-area)
@@ -151,6 +176,9 @@
                       (log/info "order cooked and placed on shelf" order shelf (if action (str "and" action affected-order) "") pick-up-area)
                       [pick-up-area (assoc couriers (order :id) courier) orders-ended]))
 
+                   ;; Handle incoming couriers. If this courier is the last
+                   ;; one left in flight and no more orders are expected, we
+                   ;; can close the delivery-ch
                    pickup-ch
                    ([order]
                     (log/info "courier arrived" order pick-up-area)
@@ -168,7 +196,7 @@
                    ;; A signal on this channel indicates that no more orders
                    ;; are coming. This gives us a chance to rendezvous with
                    ;; the caller. Once we are done waiting for the last
-                   ;; couriers we can close delivery-ch
+                   ;; couriers we can close delivery-ch.
                    end-orders-ch (do
                                    (log/info "no more orders coming," (count couriers) "couriers in flight")
                                    (if (empty? couriers)
--- test/clj/kitchen_test.clj
+++ test/clj/kitchen_test.clj
@@ -1,47 +1,9 @@
 (ns kitchen.kitchen-test
   (:require [kitchen.kitchen :as sut]
+            [kitchen.customer :as customer]
             [kitchen.config :as config]
-            [clojure.test :as t]))
-
-(t/deftest pickup-order-test
-  (t/testing "order on expected pick-up-area"
-    (let [id 1
-          order {:temp "hot" :id id}
-          pick-up-area (update-in (sut/make-empty-pick-up-area) ["hot"] assoc id order)]
-      (t/is (= [(sut/make-empty-pick-up-area) (assoc order :pickup-successful true)]
-               (sut/pickup-order pick-up-area order))))
-    (let [id 1
-          order {:temp "cold" :id id}
-          pick-up-area (update-in (sut/make-empty-pick-up-area) ["cold"] assoc id order)]
-      (t/is (= [(sut/make-empty-pick-up-area) (assoc order :pickup-successful true)]
-               (sut/pickup-order pick-up-area order))))
-    (let [id 1
-          order {:temp "cold" :id id}
-          pick-up-area-without-order (-> (sut/make-empty-pick-up-area)
-                                         (update-in ["cold"] assoc 2 {:temp "cold" :id 2})
-                                         (update-in ["cold"] assoc 3 {:temp "cold" :id 3})
-                                         (update-in ["cold"] assoc 4 {:temp "cold" :id 4})
-                                         (update-in ["hot"] assoc 5 {:temp "hot" :id 5}))
-          pick-up-area-with-order (update-in pick-up-area-without-order ["cold"] assoc id order)]
-      (t/is (= [pick-up-area-without-order (assoc order :pickup-successful true)]
-               (sut/pickup-order pick-up-area-with-order order)))))
-  (t/testing "order on overflow pick-up-area"
-    (let [id 1
-          order {:temp "hot" :id id}
-          pick-up-area (update-in (sut/make-empty-pick-up-area) ["overflow"] assoc id order)]
-      (t/is (= [(sut/make-empty-pick-up-area) (assoc order :pickup-successful true)]
-               (sut/pickup-order pick-up-area order))))
-    (let [id 1
-          order {:temp "cold" :id id}
-          pick-up-area (update-in (sut/make-empty-pick-up-area) ["overflow"] assoc id order)]
-      (t/is (= [(sut/make-empty-pick-up-area) (assoc order :pickup-successful true)]
-               (sut/pickup-order pick-up-area order)))))
-  (t/testing "order not in pick-up-area"
-    (let [id 1
-          order {:temp "hot" :id id}
-          pick-up-area (sut/make-empty-pick-up-area)]
-      (t/is (= [(sut/make-empty-pick-up-area) (assoc order :pickup-successful false)]
-               (sut/pickup-order pick-up-area order))))))
+            [clojure.test :as t]
+            [clojure.core.async :as async]))
 
 (defn- gen-shelf
   "Helper function to generate shelves for testing"
@@ -50,13 +12,100 @@
         (repeatedly n #(let [id (str (java.util.UUID/randomUUID))]
                          [id {:temp temp :id id}]))))
 
+(t/deftest deliveries-returned
+  (t/testing "one order succeeds"
+    (let [order {:temp "hot" :id 1}]
+      (t/is (= [(-> order
+                    (assoc :cooked true)
+                    (assoc :pickup-successful true))]
+               (async/<!!
+                (customer/place-orders-collect-deliveries
+                 (sut/kitchen (async/chan))
+                 [order]
+                 0))))))
+  (t/testing "orders up to shelf-capacity succeed"
+    (let [orders (map #(assoc % :temp "hot")
+                      (customer/gen-orders
+                       (+ (get-in config/env [:shelf-capacity "hot"])
+                          (get-in config/env [:shelf-capacity "overflow"]))))]
+      (t/is (= (sort-by :id
+                        (map (fn [order]
+                               (-> order
+                                   (assoc :cooked true)
+                                   (assoc :pickup-successful true)))
+                             orders))
+               (sort-by :id
+                        (async/<!!
+                         (customer/place-orders-collect-deliveries
+                          (sut/kitchen (async/chan))
+                          orders
+                          0)))))))
+  (t/testing "if there are more orders than shelf-capacity, some orders get dropped"
+    (let [orders (map #(assoc % :temp "hot")
+                      (customer/gen-orders
+                       (* 2 (+ (get-in config/env [:shelf-capacity "hot"])
+                               (get-in config/env [:shelf-capacity "overflow"])))))
+          deliveries (async/<!!
+                      (customer/place-orders-collect-deliveries
+                       (sut/kitchen (async/chan))
+                       orders
+                       0))]
+      (t/is (< (count deliveries) (count orders)))
+      (t/is (clojure.set/subset?
+             (set deliveries)
+             (set (map (fn [order]
+                         (-> order
+                             (assoc :cooked true)
+                             (assoc :pickup-successful true)))
+                       orders)))))))
+
+(t/deftest pickup-order-test
+  (t/testing "order on expected pick-up-area"
+    (let [id 1
+          order {:temp "hot" :id id}
+          pick-up-area (update-in (#'sut/make-empty-pick-up-area) ["hot"] assoc id order)]
+      (t/is (= [(#'sut/make-empty-pick-up-area) (assoc order :pickup-successful true)]
+               (#'sut/pickup-order pick-up-area order))))
+    (let [id 1
+          order {:temp "cold" :id id}
+          pick-up-area (update-in (#'sut/make-empty-pick-up-area) ["cold"] assoc id order)]
+      (t/is (= [(#'sut/make-empty-pick-up-area) (assoc order :pickup-successful true)]
+               (#'sut/pickup-order pick-up-area order))))
+    (let [id 1
+          order {:temp "cold" :id id}
+          pick-up-area-without-order (-> (#'sut/make-empty-pick-up-area)
+                                         (update-in ["cold"] assoc 2 {:temp "cold" :id 2})
+                                         (update-in ["cold"] assoc 3 {:temp "cold" :id 3})
+                                         (update-in ["cold"] assoc 4 {:temp "cold" :id 4})
+                                         (update-in ["hot"] assoc 5 {:temp "hot" :id 5}))
+          pick-up-area-with-order (update-in pick-up-area-without-order ["cold"] assoc id order)]
+      (t/is (= [pick-up-area-without-order (assoc order :pickup-successful true)]
+               (#'sut/pickup-order pick-up-area-with-order order)))))
+  (t/testing "order on overflow pick-up-area"
+    (let [id 1
+          order {:temp "hot" :id id}
+          pick-up-area (update-in (#'sut/make-empty-pick-up-area) ["overflow"] assoc id order)]
+      (t/is (= [(#'sut/make-empty-pick-up-area) (assoc order :pickup-successful true)]
+               (#'sut/pickup-order pick-up-area order))))
+    (let [id 1
+          order {:temp "cold" :id id}
+          pick-up-area (update-in (#'sut/make-empty-pick-up-area) ["overflow"] assoc id order)]
+      (t/is (= [(#'sut/make-empty-pick-up-area) (assoc order :pickup-successful true)]
+               (#'sut/pickup-order pick-up-area order)))))
+  (t/testing "order not in pick-up-area"
+    (let [id 1
+          order {:temp "hot" :id id}
+          pick-up-area (#'sut/make-empty-pick-up-area)]
+      (t/is (= [(#'sut/make-empty-pick-up-area) (assoc order :pickup-successful false)]
+               (#'sut/pickup-order pick-up-area order))))))
+
 (t/deftest put-on-pick-up-area-test
   (t/testing "place on empty pick-up-area"
     (let [order {:temp "hot" :id 1}
           pick-up-area {"hot" {1 order} "cold" {} "frozen" {} "overflow" {}}]
       (t/is (= {:pick-up-area pick-up-area
                 :shelf "hot"}
-               (sut/put-on-pick-up-area (sut/make-empty-pick-up-area) order)))))
+               (#'sut/put-on-pick-up-area (#'sut/make-empty-pick-up-area) order)))))
   (t/testing "place on shelf with some contents"
     (let [order {:temp "hot" :id 1}
           pick-up-area-before-order {"hot" {2 {:temp "hot" :id 2}, 3 {:temp "hot" :id 3}}
@@ -65,7 +114,7 @@
                               "cold" {} "frozen" {} "overflow" {}}]
       (t/is (= {:pick-up-area pick-up-area-after-order
                 :shelf "hot"}
-               (sut/put-on-pick-up-area pick-up-area-before-order order)))))
+               (#'sut/put-on-pick-up-area pick-up-area-before-order order)))))
   (t/testing "place on overflow shelf"
     (let [order {:temp "hot" :id :placed}
           hot-shelf (gen-shelf "hot" (get-in config/env [:shelf-capacity "hot"]))
@@ -73,13 +122,13 @@
           pick-up-area-after-order  {"hot" hot-shelf "cold" {} "frozen" {} "overflow" {:placed order}}]
       (t/is (= {:pick-up-area pick-up-area-after-order
                 :shelf "overflow"}
-               (sut/put-on-pick-up-area pick-up-area-before-order order)))))
+               (#'sut/put-on-pick-up-area pick-up-area-before-order order)))))
   (t/testing "move item off overflow shelf"
     (let [order {:temp "hot" :id :placed}
           hot-shelf (gen-shelf "hot" (get-in config/env [:shelf-capacity "hot"]))
           overflow-shelf (gen-shelf "cold" (get-in config/env [:shelf-capacity "overflow"]))
           pick-up-area {"hot" hot-shelf "cold" {} "frozen" {} "overflow" overflow-shelf}
-          pick-up-area-after-order (sut/put-on-pick-up-area pick-up-area order)]
+          pick-up-area-after-order (#'sut/put-on-pick-up-area pick-up-area order)]
       (t/is (= 1 (count (get-in pick-up-area-after-order [:pick-up-area "cold"]))))
       (t/is (= order (get-in pick-up-area-after-order [:pick-up-area "overflow" :placed])))
       (t/is (= hot-shelf ((:pick-up-area pick-up-area-after-order) "hot")))))
@@ -92,7 +141,7 @@
                           (gen-shelf "cold" (dec (get-in config/env [:shelf-capacity "overflow"])))
                           (gen-shelf "frozen" 1))
           pick-up-area {"hot" hot-shelf "cold" cold-shelf "frozen" frozen-shelf "overflow" overflow-shelf}
-          pick-up-area-after-order (sut/put-on-pick-up-area pick-up-area order)]
+          pick-up-area-after-order (#'sut/put-on-pick-up-area pick-up-area order)]
       (t/is (= hot-shelf (get-in pick-up-area-after-order [:pick-up-area "hot"])))
       (t/is (= cold-shelf (get-in pick-up-area-after-order [:pick-up-area "cold"])))
       (t/is (= (inc (count frozen-shelf)) (count (get-in pick-up-area-after-order [:pick-up-area "frozen"]))))
@@ -104,7 +153,7 @@
           cold-shelf (gen-shelf "cold" (get-in config/env [:shelf-capacity "cold"]))
           overflow-shelf (gen-shelf "cold" (get-in config/env [:shelf-capacity "overflow"]))
           pick-up-area {"hot" hot-shelf "cold" cold-shelf "frozen" {} "overflow" overflow-shelf}
-          pick-up-area-after-order (sut/put-on-pick-up-area pick-up-area order)]
+          pick-up-area-after-order (#'sut/put-on-pick-up-area pick-up-area order)]
       (t/is (= cold-shelf (get-in pick-up-area-after-order [:pick-up-area "cold"])))
       (t/is (= hot-shelf (get-in pick-up-area-after-order [:pick-up-area "hot"])))
       (t/is (= (pick-up-area "frozen") (get-in pick-up-area-after-order [:pick-up-area "frozen"])))
