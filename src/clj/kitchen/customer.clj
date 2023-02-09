b3d8019260fb36e2b773d153d9e28bc490da9c3f
guaqueta <Camilo Guaqueta>
2023-02-09 01:28:56 -0800

    Update customer to not depend on kitchen namespace. Just take kitchens as arguments instead

--- src/clj/kitchen/customer.clj
+++ src/clj/kitchen/customer.clj
@@ -10,8 +10,7 @@
    [clojure.core.async :as async]
    [clojure.data.json :as json]
    [clojure.java.io :as io]
-   [kitchen.config :as config]
-   [kitchen.kitchen :as kitchen]))
+   [kitchen.config :as config]))
 
 (def orders
   "Reads orders from the resource specified in the config."
@@ -19,24 +18,52 @@
    (slurp (io/resource (config/env :orders-json)))
    :key-fn keyword))
 
-(defn gen-orders 
+(defn gen-orders
   "This is just a helper function for interactive testing. Using `orders` as a
   reference, it generates `n` orders with random ids"
   [n]
   (take n (repeatedly (fn [] (assoc (rand-nth orders) :id (str (java.util.UUID/randomUUID)))))))
 
 (defn place-orders
-  "Emulates customers placing orders on the system. Orders are placed
+  "Emulates customers placing orders on the kitchen. Orders are placed
   asynchronously (i.e. without waiting for a reply), at a fixed rate governed
-  by config/env :customer-wait-between-orders. Returns immediately.
+  by config/env :customer-wait-between-orders. Returns immediately. Doesn't
+  wait for deliveries to return.
+
+  With two arguments, the wait-time between orders defaults
+  to (config/env :customer-wait-between-orders).
 
   Note we deliberately don't respect back-pressure (i.e. we use async/put! to
   place orders). This is because we are emulating orders coming in at a rate
   which the rest of the system cannot control. In other words, the kitchen can
-  drop orders, but it can't ask customers to wait."
-  [orders]
-  (let [wait-time (config/env :customer-wait-between-orders)]
-    (async/go
-      (doseq [order orders]
-        (async/<!! (async/timeout wait-time))
-        (async/put! kitchen/orders-ch order)))))
+  drop orders, but it can't ask customers to slow down."
+  ([kitchen orders] (place-orders kitchen orders (config/env :customer-wait-between-orders)))
+  ([kitchen orders wait-time] 
+   (async/go
+     (doseq [order orders]
+       (async/<!! (async/timeout wait-time))
+       (async/put! (kitchen :orders) order)))))
+
+(defn place-orders-collect-deliveries
+  "Emulates customers placing orders on the system. Orders are placed
+  asynchronously (i.e. without waiting for a reply), at a fixed rate governed
+  by config/env :customer-wait-between-orders. Returns immediately.
+
+  With two arguments, the wait-time between orders defaults
+  to (config/env :customer-wait-between-orders).
+
+  After placing all orders, indicates to the kitchen that no more orders are
+  coming, and then collects all deliveries into a coll. Returns a chan where
+  the coll of deliveries can be returned. Note that this means the kitchen
+  passed in will be unable to accept new orders."
+  ([kitchen orders] (place-orders-collect-deliveries kitchen orders (config/env :customer-wait-between-orders)))
+  ([kitchen orders wait-time]
+   (async/go
+     (doseq [order orders]
+       (async/<!! (async/timeout wait-time))
+       (async/put! (kitchen :orders) order))
+     (async/>! (kitchen :end-orders) true))
+   (async/go-loop [accepted []]
+     (if-let [delivery (async/<! (kitchen :delivery))]
+       (recur (conj accepted delivery))
+       accepted))))
